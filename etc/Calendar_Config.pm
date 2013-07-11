027697d8c845c7e8cfe4dfb06ceb76719436c171
Bradley Bell <bradleyb@uw.edu>
2013-07-10 18:40:55 -0700

    add config for box format

--- etc/Calendar_Config.pm
+++ etc/Calendar_Config.pm
@@ -8,15 +8,26 @@ etc/RT_SiteConfig.pm :
 
 
 
-=item C<$DefaultCalendarFormat>
+=item C<$DefaultCalendarBoxFormat>
 
-C<$DefaultCalendarFormat> is the default display format for calendar
-entries. The first element is displayed on the calendar itself, the
-rest is shown only in the popup.
+C<$DefaultCalendarBoxFormat> is the default display format for the
+calendar boxes. It's just like a search format, but it's only a single
+element.
 
 =cut
 
-Set ($DefaultCalendarFormat, qq{
+Set($DefaultCalendarBoxFormat, '__QueueName__ #__id__ __Subject__');
+
+
+=item C<$DefaultCalendarFormat>
+
+C<$DefaultCalendarFormat> is the default display format for calendar
+popups. The date fields present also determine whether a ticket will
+be displayed on a given day.
+
+=cut
+
+Set($DefaultCalendarFormat, qq{
    '<a href="__WebPath__/Ticket/Display.html?id=__id__"><strong>__QueueName__ #__id__ :</strong> __Subject__</a>/TITLE:',
    '__NEWLINE__',
    OwnerName,
